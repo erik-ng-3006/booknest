54ed01bc3093a20d4a48fee8c9dfedfd15d98887
erik-ng-3006 <erikng3006@gmail.com>
2025-04-04 10:50:09 +0700

    implement footer section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
 	return (
@@ -9,7 +10,7 @@ const App = () => {
 			<main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
 				<Outlet />
 			</main>
-			<footer>Footer</footer>
+			<Footer />
 		</>
 	);
 };
