aa9ff2ffde6ed191815c4086228f0cdf964dc68d
erik-ng-3006 <erikng3006@gmail.com>
2025-04-07 21:37:38 +0700

    fix missing import and wrong url path

--- frontend/src/routers/AdminRoute.jsx
+++ frontend/src/routers/AdminRoute.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const AdminRoute = ({ children }) => {
 	const token = localStorage.getItem('token');
 
 	if (!token) {
-		return <Navigate to='/login' />;
+		return <Navigate to='/admin' />;
 	}
 
 	return children ? children : <Outlet />;
